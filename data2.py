a277f6366aebe01ca1444179d4afee138df3f307
Tassio Ferenzini Martins Sirqueira <tassio@tassio.eti.br>
2017-05-12 18:44:20 -0300

    Corrreção
    Python 3

--- data2.py
+++ data2.py
@@ -7,33 +7,33 @@ import CPUtimer
 def parse_instance(instance_file):
     
     with open(instance_file) as f:
-		 text = f.readlines()
-		 lines = [t.strip().split() for t in text]
-		 nb_items = int(lines[0][0])
-		 P = [0] * nb_items
-		 W = [0] * nb_items
-		 
-		 for line in lines[1:]:
-			 #valores = text.split()
-			 if line[1:2]:
-				 index = long(line[0])
-				 profit = long(line[1])
-				 weight = long(line[2])
-				 #print('index', index, "profit", profit, "weight", weight)
-				 P[index - 1] = profit
-				 W[index - 1] = weight
-			 else:
-				knapsack_size = long(line[0])
-				#print('knapsack_size', knapsack_size)
-		 return knapsack_size, P, W
+         text = f.readlines()
+         lines = [t.strip().split() for t in text]
+         nb_items = int(lines[0][0])
+         P = [0] * nb_items
+         W = [0] * nb_items
+         
+         for line in lines[1:]:
+             #valores = text.split()
+             if line[1:2]:
+                 index = int(line[0])
+                 profit = int(line[1])
+                 weight = int(line[2])
+                 #print('index', index, "profit", profit, "weight", weight)
+                 P[index - 1] = profit
+                 W[index - 1] = weight
+             else:
+                knapsack_size = int(line[0])
+                #print('knapsack_size', knapsack_size)
+         return knapsack_size, P, W
 
 def instance_iterator(instance_path):
     file_list = [f for f in os.listdir(instance_path)
     if f.startswith('m') and f.endswith('.in')]
     for filename in sorted(file_list):
-		path = os.path.join(instance_path, filename)
-		k, P, W = parse_instance(path)
-		yield (filename[1:-3], k, P, W)
+        path = os.path.join(instance_path, filename)
+        k, P, W = parse_instance(path)
+        yield (filename[1:-3], k, P, W)
 
 def _print_solution(nb_items_used, total_weight, total_profit, items_fractions, instance_name, question, timer):
 
@@ -45,7 +45,7 @@ def _print_solution(nb_items_used, total_weight, total_profit, items_fractions, 
 
     output = open((os.getcwd()+"/Output/Question"+str(question))+"/"+"m"+instance_name+".in", "w")
     
-    output.write('{} \t{} \t{} \n'.format('Number of items','Total Weight',	'Total Profit'))
+    output.write('{} \t{} \t{} \n'.format('Number of items','Total Weight',    'Total Profit'))
     output.write('{} \t{} \t{} \n'.format(nb_items_used, total_weight, total_profit))
     
     output.write("\n")
@@ -58,17 +58,18 @@ def _print_solution(nb_items_used, total_weight, total_profit, items_fractions, 
     output.write("\n" + timer.get_stamp("total","si",True))
     output.write("\n" + timer.get_stamp("total","clock",True))
     output.write("\n\n")
-    output.write("Item	Fraction\n\n")
+    output.write("Item    Fraction\n\n")
     items = sorted(items_fractions, key=lambda t: t[0])
     for item, fract in items:
         output.write('{} \t{}\n'.format(item, fract))
 
 def print_solution(P, W, items_fractions, instance_name, question, timer):
-    ''''
+    
     nb_items_used = len(items_fractions)
     total_weight = sum(W[item - 1] * fract for item, fract in items_fractions)
     total_profit = sum(P[item - 1] * fract for item, fract in items_fractions)
     _print_solution(nb_items_used, total_weight, total_profit, items_fractions, instance_name, question, timer)
+    '''
     print("Total time: " + str( timer.get_time() ) +" s")
     print("Average time: " + str( timer.get_time("average","micro") ) +" \u00B5s")
     print("Last call: " + str( timer.get_time("last","micro") ) +" \u00B5s")
@@ -77,4 +78,4 @@ def print_solution(P, W, items_fractions, instance_name, question, timer):
     print("\nPattern that ignores zeros:")
     print( timer.get_stamp("total","si",True) )
     print( timer.get_stamp("total","clock",True) )
-	'''
+    '''
--- question2a.py
+++ question2a.py
@@ -6,16 +6,16 @@ import CPUtimer
 from data2 import instance_iterator, print_solution
 
 def solve(instance_path):
-	timer = CPUtimer.CPUTimer()
-	#print('Question 2 a\n')
-	for instance in instance_iterator(instance_path):
-		instance_name, k, P, W = instance
-		#print('Solving', instance_name)
-		timer.reset()
-		timer.start()
-		items = knapsack(k, P, W)
-		timer.stop()
-		print_solution(P, W, items, instance_name, '2a', timer)
+    timer = CPUtimer.CPUTimer()
+    #print('Question 2 a\n')
+    for instance in instance_iterator(instance_path):
+        instance_name, k, P, W = instance
+        #print('Solving', instance_name)
+        timer.reset()
+        timer.start()
+        items = knapsack(k, P, W)
+        timer.stop()
+        print_solution(P, W, items, instance_name, '2a', timer)
 
 def knapsack(k, P, W):
     items = [(i + 1, w, p / w) for i, (p, w) in enumerate(zip(P, W))]
@@ -24,15 +24,15 @@ def knapsack(k, P, W):
     x = [0] * len(P)
     weight = 0
     while weight < k:
-		try:
-			i, w, _ = next(items)
-			if weight + w <= k:
-				x[i - 1] = 1
-				weight += w
-			else:
-				x[i - 1] = (k - weight) / w
-				weight = k
-		except Exception as e:
-			#print (e)
-			break
+        try:
+            i, w, _ = next(items)
+            if weight + w <= k:
+                x[i - 1] = 1
+                weight += w
+            else:
+                x[i - 1] = (k - weight) / w
+                weight = k
+        except Exception as e:
+            #print (e)
+            break
     return [ (i + 1, fract) for i, fract in enumerate(x) if fract > 0]
