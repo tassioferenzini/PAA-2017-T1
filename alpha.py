925c36b0d11a66bdb585f43b5050dedbb2ce37a0
Tassio Ferenzini Martins Sirqueira <tassio@tassio.eti.br>
2017-06-01 10:35:23 -0300

    Correções no CPUTIME 2
    Registrando o time em segundos e outputs

--- alpha.py
+++ alpha.py
@@ -360,7 +360,7 @@ class Graph:
 if __name__ == '__main__':
 
     timer = CPUtimer.CPUTimer()
-    instance_path = "ALUE/"
+    instance_path = "falta/"
 
     file_list = [f for f in os.listdir(instance_path)
                  if f.endswith('.stp')]
